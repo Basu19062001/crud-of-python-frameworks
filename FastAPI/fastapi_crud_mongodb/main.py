6bdc2ea7c9f3e5caf773200106af92ec32f7bbf2
Basudev Samanta <samantabasu.2001@gmail.com>
2025-06-21 12:57:26 +0000

    fix: add user operations endpoint with prefix

--- FastAPI/fastapi_crud_mongodb/main.py
+++ FastAPI/fastapi_crud_mongodb/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 
 from routes.user_auth import routes
+from routes.user_operation import opt_routes
 
 app = FastAPI()
 
 app.include_router(routes, prefix="/api/v1")
+app.include_router(opt_routes, prefix="/api/v1") 
 
 @app.get("/")
 async def root():
